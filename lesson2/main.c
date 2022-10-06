257a80707d68b1f86af6e4bac222c040c0732ab2
DESKTOP-PFIKTDD\danwo <dan.wolf@icloud.com>
2022-10-05 22:02:52 -0400

    Initial commit of first 5 lessons

---
+++ lesson2/main.c
@@ -0,0 +1,15 @@
+
+int main() {  
+    int counter = 0;
+    while (counter < 21) {
+        ++counter;
+        if ((counter & 1) != 0) {
+            // Do Something 
+        }
+        else {
+            // Do something else 
+        }
+    }
+    
+    return 0;
+}
